700096ef8b7b3b8053dd369346e3323cc2d01adf
Sunny Dhama <>
2024-08-27 10:05:29 -0400

    gpx changes

--- models/Route.js
+++ models/Route.js
@@ -1,48 +1,52 @@
-const { model, Schema } = require("mongoose");
+const { model, Schema } = require('mongoose');
 
 const routeSchema = new Schema({
-    points: {
-        type: [[Number]],
-        default: [],
-        required: true,
-    },
-    elevation: {
-        type: [Number],
-        default: [],
-        required: true,
-    },
-    grade: {
-        type: [Number],
-        default: [],
-    },
-    terrain: {
-        type: [String],
-        default: [],
-    },
-    distance: {
-        type: Number,
-        required: true,
-    },
-    maxElevation: {
-        type: Number,
-        required: true,
-    },
-    minElevation: {
-        type: Number,
-        required: true,
-    },
-    totalElevationGain: {
-        type: Number,
-        required: true,
-    },
-    startCoordinates: {
-        type: [Number],
-        required: true,
-    },
-    endCoordinates: {
-        type: [Number],
-        required: true,
-    },
+  points: {
+    type: [[Number]],
+    default: [],
+    required: true,
+  },
+  elevation: {
+    type: [Number],
+    default: [],
+    required: false,
+    default: null,
+  },
+  grade: {
+    type: [Number],
+    default: [],
+  },
+  terrain: {
+    type: [String],
+    default: [],
+  },
+  distance: {
+    type: Number,
+    required: true,
+  },
+  maxElevation: {
+    type: Number,
+    required: false,
+    default: null,
+  },
+  minElevation: {
+    type: Number,
+    required: false,
+    default: null,
+  },
+  totalElevationGain: {
+    type: Number,
+    required: false,
+    default: null,
+  },
+  startCoordinates: {
+    type: [Number],
+    required: true,
+  },
+  endCoordinates: {
+    type: [Number],
+    required: true,
+  },
 });
 
 module.exports = model('Route', routeSchema);
